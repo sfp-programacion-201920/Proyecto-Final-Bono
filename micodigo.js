a66ec20bfa9ae6173ce20619394ffbf080bf3d2e
manbontor <manuelbonotorres@gmail.com>
2020-02-22 15:46:34 +0100

    cosas

--- micodigo.js
+++ micodigo.js
@@ -1,2 +1,19 @@
-// alert("bienvenidos a mi página de comida");
-// comentario 
+
+function validar_nombre() {
+    var nombre= document.forms["formulario"]["nombre"].value;
+
+    if (nombre == null || nombre == "") {
+        alert("El nombre no puede estar en blanco");
+        return false;
+
+    }
+}
+
+function validar_pass() {
+    if ((document.getElementById("Contraseña").value.length < 4) ||
+    (document.getElementById("Contraseña").value.length) > 20 ) {
+        alert("la clave tiene que tener entre 4 y 20 caracteres")
+    }
+    else
+        alert("Todo es correcto")
+};
