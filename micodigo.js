03bdf257e8123191af46cc39dd451ab008460793
manbontor <manuelbonotorres@gmail.com>
2020-03-09 18:14:08 +0100

    ddd

--- micodigo.js
+++ micodigo.js
@@ -1,14 +1,43 @@
 
 function validar_nombre() {
-    var nombre= document.forms["formulario"]["nombre"].value;
-
-    if (nombre == null || nombre == "") {
-        alert("El nombre no puede estar en blanco");
+    var nombre = document.forms["formulario"]["nombre"].value;
+    if (nombre == null || nombre == 0) {
+        alert("Ingrese el nombre");
         return false;
+    };
+    var nombre = document.forms["formulario"]["Apellidos"].value;
+    if (nombre == null || nombre == 0) {
+        alert("Ingrese el Apellido");
+        return false;
+    };
+    var nombre = document.forms["formulario"]["opiniones"].value;
+    if (nombre == null || nombre == 0) {
+        alert("Añada opiniones");
+        return false;
+    };
+    var nombre = document.forms["formulario"]["Apodo"].value;
+    if (nombre == null || nombre == 0) {
+        alert("Ingrese el Apodo");
+        return false;
+    };
+    var nombre = document.forms["formulario"]["contrasena"].value;
+    if (nombre == null || nombre == 0) {
+        alert("Ingrese la contraseña");
+        return false;
+    };
+    var nombre = document.forms["formulario"]["edad"].value;
+    if (nombre == null || nombre == 0) {
+        alert("Ingrese la edad");
+        return false;
+    };
+};  
 
-    }
-}
 
+
+
+
+
+  
 function validar_select() {
     indice = document.getElementById("opciones").value;
     if (indice == null || indice == 0) {
@@ -25,11 +54,5 @@ function numerico() {
     }
 }
 
-function vacio(control) {
-    control.value == "";
-}
-function verificar(control) {
-    if (control.value == "")
-        alert("debe ingresar datos");
-}
+
 
